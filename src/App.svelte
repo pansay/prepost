d2f0e138b2c47b5163b365542b3a0cebbe97906c
pansay <jan.krepelka@gmail.com>
2021-01-06 11:42:07 +0100

    Add formatting from date

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,31 @@
 <script>
   import prettyUrl from './pretty-url.js';
-  let name = '';
+  import prettyFilename from './prettyFilename.js';
+  let title = '';
+  let isoDate = new Date().toISOString().substring(0, 10);
+
+  let prettyUrlFromTitle = '';
+  $: prettyUrlFromTitle = prettyUrl(title)
+
+  let prettyFilenameFromTitleAndDate = '';
+  $: prettyFilenameFromTitleAndDate = prettyFilename(prettyUrlFromTitle, isoDate)
 </script>
 
 <main>
-  <input bind:value={name}>
-  <h1>{prettyUrl(name)}</h1>
+  <h1>prePost</h1>
+  <div>
+    <label>date: </label><input type="date" bind:value={isoDate}>
+    <label>title: </label><input bind:value={title}>
+    <label>pretty-url: </label><input bind:value={prettyUrlFromTitle}>
+    <label>filename: </label><input type="text" bind:value={prettyFilenameFromTitleAndDate} disabled>
+  </div>
 </main>
+
+<footer>
+  <a href="https://github.com/pansay/prepost">
+    source code and documentation on GitHub
+  </a>
+</footer>
 
 <style>
   main {
---
+++ src/prettyFilename.js
@@ -0,0 +1,5 @@
+const prettyFilename= (title, isoDate) => {
+  return `${isoDate}_${title}.md`;
+}
+
+export default prettyFilename;
