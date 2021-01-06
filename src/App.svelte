c33b0d0b616e82f937e4f26ebab9ec4c552e4741
pansay <jan.krepelka@gmail.com>
2021-01-07 00:44:18 +0100

    Fix exports

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,24 @@
   import prettyUrl from './lib/pretty-url.js';
   import prettyFilename from './lib/prettyFilename.js';
 
+  const selectRichText = (elementId) => {
+    const node = document.getElementById(elementId);
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
+    }
+    return node;
+  }
+
   const convertMarkdown = (markdownString) => {
     return showDownConverter.makeHtml(markdownString);
   }
@@ -14,25 +32,43 @@
     return text.substring(0, index);
   }
 
-  const copyOnClick = (element) => {
-    element.select();
+  const copyOnClick = (text) => {
     document.execCommand('copy');
-    alert(`Copied '${element.value}' to clipboard!`);
+    alert(`Copied\n\n${text}\n\nto clipboard!`);
   }
 
   const copyFilenameToClipboard = () => {
     const element = document.getElementById('filename');
-    copyOnClick(element);
+    element.select();
+    copyOnClick(element.value);
+  }
+
+  const copyMarkdownToClipboard = () => {
+    const element = document.getElementById('markdown');
+    element.select();
+    copyOnClick(element.value);
+  }
+
+  const copySourceHTMLToClipboard = () => {
+    const element = document.getElementById('sourceHTML');
+    element.select();
+    copyOnClick(element.value);
+  }
+
+  const copyRenderedHTMLToClipboard = () => {
+    // this one is different as it's not textarea
+    const element = selectRichText('renderedHTML');
+    copyOnClick('rendered\n\n' + element.innerHTML);
   }
 
   const saveFileMarkdown = (fileContent, fileName) => {
     var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
-    FileSaver.saveAs(blob, fileName);
+    FileSaver.saveAs(blob, fileName); // .md file
   }
 
   const saveFileHTML = (fileContent, fileName) => {
     var blob = new Blob([fileContent], {type: "text/html;charset=utf-8"});
-    FileSaver.saveAs(blob, fileName);
+    FileSaver.saveAs(blob, fileName); // .partial.html file
   }
 
   const showDownConverter = new showdown.Converter();
@@ -50,7 +86,7 @@
   $: prettyFilenameMarkdown = prettyFilename(prettyUrlFromTitle, isoDate, 'md');
 
   let prettyFilenameHTML = '';
-  $: prettyFilenameHTML = prettyFilename(prettyUrlFromTitle, isoDate, 'html');
+  $: prettyFilenameHTML = prettyFilename(prettyUrlFromTitle, isoDate, 'partial.html');
 
   let html = '';
   $: html = convertMarkdown(text);
@@ -62,15 +98,13 @@
   <div class="columns">
 
     <div class="column">
-      <label>markdown text input<br>(title is first line)</label>
-      <textarea bind:value={text}></textarea>
+      <label>Markdown text input<br>(title is first line)</label>
+      <textarea
+        bind:value={text}
+        id="markdown" />
       <p>
         <button
-          on:click={saveFileMarkdown(text, prettyFilenameMarkdown)}>
-          save Markdown file
-        </button>
-        <button
-          on:click={copyToClipboard(html)}>
+          on:click={copyMarkdownToClipboard}>
           copy Mardown text to clipboard
         </button>
       </p>
@@ -78,15 +112,13 @@
 
     <div class="column">
       <label>HTML<br>code output</label>
-      <textarea bind:value={html}></textarea>
+      <textarea
+        bind:value={html}
+        id="sourceHTML" />
 
       <p>
         <button
-          on:click={saveFileHTML(html, prettyFilenameHTML)}>
-          save HTML file
-        </button>
-        <button
-          on:click={copyToClipboard(html)}>
+          on:click={copySourceHTMLToClipboard}>
           copy HTML source to clipboard
         </button>
       </p>
@@ -94,17 +126,21 @@
 
     <div class="column">
       <label>HTML<br>rendered output</label>
-      <div class="rendered">{@html html}</div>
+      <div
+        id="renderedHTML"
+        class="rendered">
+        {@html html}
+      </div>
       <p>
         <button
-          on:click={copyToClipboard(html)}>
+          on:click={copyRenderedHTMLToClipboard}>
           copy rendered HTML to clipboard
         </button>
       </p>
     </div>
 
     <div class="column">
-      <label>post date: </label><input type="date" bind:value={isoDate}>
+      <label>post date<br>(default today): </label><input type="date" bind:value={isoDate}>
 
       <label>title: </label><input bind:value={title}>
 
@@ -114,13 +150,23 @@
       <input
         type="text"
         id="filename"
-        bind:value={prettyFilenameMarkdown}
-        readonly>
+        bind:value={prettyFilenameMarkdown}>
 
       <p>
         <button
           on:click={copyFilenameToClipboard}>
           copy filename to clipboard
+        </button>
+      </p>
+
+      <p>
+        <button
+          on:click={saveFileMarkdown(text, prettyFilenameMarkdown)}>
+          save Markdown file
+        </button>
+        <button
+          on:click={saveFileHTML(html, prettyFilenameHTML)}>
+          save HTML (partial)
         </button>
       </p>
 
