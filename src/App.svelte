da1c494a1130c9d4ed2e5fa5acf1c4f235ea95bb
pansay <jan.krepelka@gmail.com>
2021-01-06 12:16:45 +0100

    Finalize file formatting

--- src/App.svelte
+++ src/App.svelte
@@ -5,19 +5,33 @@
   let isoDate = new Date().toISOString().substring(0, 10);
 
   let prettyUrlFromTitle = '';
-  $: prettyUrlFromTitle = prettyUrl(title)
+  $: prettyUrlFromTitle = prettyUrl(title);
 
   let prettyFilenameFromTitleAndDate = '';
-  $: prettyFilenameFromTitleAndDate = prettyFilename(prettyUrlFromTitle, isoDate)
+  $: prettyFilenameFromTitleAndDate = prettyFilename(prettyUrlFromTitle, isoDate);
+
+  const copyOnClick = function() {
+    this.select();
+    document.execCommand('copy');
+    alert(`Copied '${this.value}' to clipboard!`);
+  }
 </script>
 
 <main>
   <h1>prePost</h1>
   <div>
     <label>date: </label><input type="date" bind:value={isoDate}>
+
     <label>title: </label><input bind:value={title}>
+
     <label>pretty-url: </label><input bind:value={prettyUrlFromTitle}>
-    <label>filename: </label><input type="text" bind:value={prettyFilenameFromTitleAndDate} disabled>
+
+    <label>filename: </label>
+    <input
+      type="text"
+      bind:value={prettyFilenameFromTitleAndDate}
+      on:click={copyOnClick}
+      readonly>
   </div>
 </main>
 
